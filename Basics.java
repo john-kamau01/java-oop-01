532b68cfc7071e0f192c2448a8f45d79d65ec26c
John Kamau <johnwaweru092@gmail.com>
2025-02-05 09:17:30 +0000

    basic String methods

---
+++ Basics.java
@@ -0,0 +1,7 @@
+public class Basics{
+    public static void main(String[] args){
+        String txt = "HELLO WORLD";
+        System.out.println("The length of the txt variable is: " + txt.length()); // Length
+        System.out.println(txt.toUpperCase()); // uppercase 
+    }
+}
