7c7a5822611c9913424ef1df71f36b11a27a7381
John Kamau <johnwaweru092@gmail.com>
2025-02-05 08:13:54 +0000

    java variables

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello John. You're a Rockstar Software Engineer.");
+        // Single line comment
+
+        // VARIABLES
+        // Variables are containers for storing data values
+        int x = 10;
+        final float pie = 3.14f; // Use final to make the variable a constant
+        boolean age = true;
+        char letter = 'a';
+        String name = "John";
+        System.out.println("Hello " + name);
     }
 }
